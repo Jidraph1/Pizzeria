1d7b60945e9e43b8b0ef1486e9b8b59e69c652ae
jidraph kimachia <jidraphkimachia1@gmail.com>
2022-03-20 19:03:07 +0300

    order details

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 
 })
+
+var small = parseInt(500);
+var medium = parseInt(700);
+var large = parseInt(1000);
+
+var cheese = parseInt(300)
+var olives = parseInt (350)
+var peperoni = parseInt(500)
+
+var crispy = parseInt(500)
+var stuffed = parseInt(550)
+var glutten = parseInt(650)
